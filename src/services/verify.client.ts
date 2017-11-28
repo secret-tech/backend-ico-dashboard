711d344818572c45ebee6eda1a635ac57ec7e728
Artem Pikulin <ortgma@gmail.com>
2017-11-29 01:14:03 +0700

    Closes #49. Check what comes in error object.

--- src/services/verify.client.ts
+++ src/services/verify.client.ts
@@ -63,6 +63,8 @@ export class VerificationClient implements VerificationClientInterface {
       });
     } catch (e) {
       if (e.statusCode === 422) {
+        console.log(e);
+
         if (e.body.data.attempts >= config.verify.maxAttempts) {
           await this.invalidateVerification(method, id);
           throw new MaxVerificationsAttemptsReached('You have used all attempts to enter code');
