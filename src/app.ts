5556e409ed0b5f1b112d140762c089201e3871a0
aleserche <aleserche@gmail.com>
2018-11-01 01:42:17 +0500

    Disable Content-Type

--- src/app.ts
+++ src/app.ts
@@ -43,15 +43,6 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 app.post('*', (req: Request, res: Response, next: NextFunction) => {
-  if (
-    !req.header('Content-Type') ||
-    (req.header('Content-Type') !== 'application/json' && !req.header('Content-Type').includes('application/x-www-form-urlencoded'))
-  ) {
-    return res.status(406).json({
-      error: 'Unsupported "Content-Type"'
-    });
-  }
-
   return next();
 });
 
